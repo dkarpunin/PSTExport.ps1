f19583cbe6cc90c51e7e29ea2254b1e1ba7a8e64
Дмитрий Карпунин <dkarpunin@users.noreply.github.com>
2021-02-22 22:23:01 +0500

    Add files via upload

---
+++ PSTExport.ps1
@@ -0,0 +1,27 @@
+﻿cls
+
+$ErrorActionPreference="SilentlyContinue"
+
+Write-Host "Экспорт п/я в PST `n" -ForegroundColor Green
+
+$Email = Read-Host "Адрес п/я"
+
+$Path="\\<shared_folder_path>\"+$Email+".pst"
+
+$Domains = (Get-ADForest).Domains
+
+foreach ($Domain in $Domains) {
+   
+    $DCs = (Get-ADDomain -Server $Domain).InfrastructureMaster
+
+    foreach ($DC in $DCs) {
+        
+        $OriginatingServer = (Get-Mailbox -Identity $Email -DomainController $DC).OriginatingServer
+
+        if($OriginatingServer) {$MailboxDC = $OriginatingServer}
+
+    }
+
+}
+
+New-MailboxExportRequest -DomainController $MailboxDC -Mailbox $Email -FilePath $Path
